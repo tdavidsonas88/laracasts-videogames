9b35c5a6560be4d22f8f75c8e50acfd43ab366a2
Tadas <tdavidsonas88@gmail.com>
2020-10-25 11:04:16 +0200

    some FE stuff with tailwind css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,17 @@
             </div>
             <div class="flex items-center">
                 <div class="relative">
-                    <input type="text" class="bg-gray-800 text-sm rounded-full px-3 py-1" placeholder="Search...">
+                    <input type="text"
+                           class="bg-gray-800 text-sm rounded-full focus:outline-none
+                                    focus:shadow-outline w-64 px-3 pl-10 py-1"
+                           placeholder="Search...">
+                    <div class="absolute top-0 flex items-center h-full ml-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor"
+                             stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                             class="feather feather-search text-gray-400">
+                            <circle cx="11" cy="11" r="8"/><path d="M21 21l-4.35-4.35"/>
+                        </svg>
+                    </div>
                 </div>
                 <div class="ml-6">
                     <a href="#"><img src="/avatar.png" alt="avatar" class="rounded-full w-8"></a>
@@ -31,5 +41,16 @@
             </div>
         </nav>
     </header>
+
+    <main class="py-8">
+        @yield('content')
+    </main>
+
+    <footer class="border-t border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            Powered by <a href="#" class="underline hover:text-gray-400">IGDB API</a>
+        </div>
+    </footer>
+
 </body>
 </html>
