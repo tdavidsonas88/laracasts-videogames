47b8523ed025c0f97636c1e1cd4591791e93b3c7
Tadas <tdavidsonas88@gmail.com>
2020-10-25 10:33:45 +0200

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laracasts Video Games</title>
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <img src="/laracasts-negative-logo.svg" alt="laracasts" class="w-32 flex-none">
+                </a>
+                <ul class="flex ml-16 space-x-8">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+                </ul>
+            </div>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text" class="bg-gray-800 text-sm rounded-full px-3 py-1" placeholder="Search...">
+                </div>
+                <div class="ml-6">
+                    <a href="#"><img src="/avatar.png" alt="avatar" class="rounded-full w-8"></a>
+                </div>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
